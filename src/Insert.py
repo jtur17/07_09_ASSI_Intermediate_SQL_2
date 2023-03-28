58367817a284dcc96edf95e68bf05af6e77df498
jtur17 <jtur@cifpfbmoll.eu>
2023-03-28 19:32:01 +0200

    skdf

--- src/Insert.py
+++ src/Insert.py
@@ -5,18 +5,17 @@ db = mysql.connector.connect(
   host="172.17.0.2",
   user="root",
   password="root",
-  database="bikes_shop"
+  database="bike_shop"
 )
 
-imagen = input("¿Que imagen quieres guardar? ")
 
 # Lectura del archivo de imagen en bytes
-with open("imagenes/imagen.jpeg", "rb") as f:
+with open("blob/imagenes/imagen.jpeg", "rb") as f:
   imagen_bytes = f.read()
 
 # Consulta SQL para insertar datos BLOB en la tabla
-sql = "INSERT INTO bicicletas (id, nombre, foto) VALUES (%s, %s, %s)"
-val = (2, "pepe", imagen_bytes)
+sql = "INSERT INTO bikes (name_bike, brand, year_bike, color, size_whell, price, image) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+val = ('Mountain Bike', 'Cannondale', '2020-01-01', 'black', '29', '499.99',imagen_bytes)
 
 # Ejecución de la consulta SQL
 cursor = db.cursor()
