ede1bf12389f543ef798edc11bc6c7d3ed90a4ee
Gema Marquinez <gmarquinezquiroz@cifpfbmoll.eu>
2023-03-28 18:58:42 +0200

    feat(Insert.py) change credentials database

--- src/Insert.py
+++ src/Insert.py
@@ -1,20 +1,12 @@
 import mysql.connector
 
 # Conexión a la base de datos MariaDB
-# db = mysql.connector.connect(
-#   host="172.17.0.2",
-#   user="root",
-#   password="root",
-#   database="bikes_shop"
-# )
-db_host = 'db'
-db_port = '3307'
-db_name = 'mydatabase'
-db_user = 'myuser'
-db_password = 'mypassword'
-
-cnx = mysql.connector.connect(user=db_user, password=db_password,host=db_host, port=db_port,
-database=db_name)
+db = mysql.connector.connect(
+  host="172.17.0.2",
+  user="root",
+  password="root",
+  database="bikes_shop"
+)
 
 imagen = input("¿Que imagen quieres guardar? ")
 
--- src/update.py
+++ src/update.py
@@ -1,23 +1,12 @@
 import mysql.connector
 
 # Establish a connection to the database
-# db = mysql.connector.connect(
-#   host="172.17.0.2",
-#   user="root",
-#   passwd="root",
-#   database="bikes"
-# )
-
-db_host = 'db'
-db_port = '3307'
-db_name = 'mydatabase'
-db_user = 'myuser'
-db_password = 'mypassword'
-
-cnx = mysql.connector.connect(user=db_user, password=db_password,host=db_host, port=db_port,
-database=db_name)
-
-
+db = mysql.connector.connect(
+  host="172.17.0.2",
+  user="root",
+  passwd="root",
+  database="bikes"
+)
 # Create a cursor object
 cursor = db.cursor()
 
